c58932e33ea141dd6b039a644532029a430ee7d2
Istvan Kadar <odesk.kadari@gmail.com>
2018-12-11 11:54:27 +0100

    Add new probes

--- repository_test.php
+++ repository_test.php
@@ -50,7 +50,13 @@ $cars = [
         'brand' => [
             'name' => 'Lada',
             'code' => 'lada',
-        ]
+            'producer' => [
+                'name' => 'Lada Factory',
+                'code' => 'lada_factory',
+                'default_plate_number' => 'LADA-FAC-001'
+            ],
+//            'default_plate_number' => 'LADA-001'
+        ],
     ]
 ];
 
@@ -69,11 +75,11 @@ $t1 = microtime(true);
 //var_dump($list);
 
 $entity = end($list);
-var_dump($entity);
+//var_dump($entity);
 //var_dump($entity->get());
 
 $entity = $repository->getById($entity->id);
-var_dump($entity);
+//var_dump($entity);
 var_dump($entity->getPlateNumber());
 var_dump($entity->getColor()->getName());
 var_dump($entity->getBrand()->getName());
@@ -86,11 +92,20 @@ $yellowColor->load([
 ]);
 
 $entity->setColor($yellowColor->get());
+var_dump($entity->getColor()->getName());
 
 $repository->edit($entity);
 
 $repository->delete($entity->id);
 $list = $repository->list();
-var_dump($list);
+print_r($list);
+//var_dump($list);
+
+//$entity = $list[0];
+
+//$presenter = new \Wheel\Core\Presentation\RubricEditionPresenter($entity);
+//var_dump($presenter);
+
+//echo($presenter->getData());
 
 //var_dump(round($t1-$t0, 4));
