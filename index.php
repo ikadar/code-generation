d513878c6532726d67ed64046b94410f4e03d5b0
Istvan Kadar <odesk.kadari@gmail.com>
2019-01-08 16:39:33 +0100

    Move TODOs into index.php

--- index.php
+++ index.php
@@ -6,51 +6,8 @@
  * Time: 23:17
  */
 
-require __DIR__ . '/vendor/autoload.php';
 
-use Wheel\Core\PrototypeService;
-
-include("repository.php");
-
-$t0 = microtime(true);
-
-$oneCar = PrototypeService::new('\Wheel\Concept\Car\CarProxy');
-
-//$newColor = PrototypeService::new('\Wheel\Concept\Car\ColorProxy')
-//    ->setName('Red')
-//    ->setcode('red')
-//;
-
-$oneCar->load($repository->get('car.trabant'));
-
-var_dump($oneCar);
-var_dump($oneCar->getPlateNumber());
-var_dump($oneCar->getColor()->getName() . ' (' . $oneCar->getColor()->getCode() . ')');
-var_dump($oneCar->getBrand()->getName() . ' (' . $oneCar->getBrand()->getCode() . ')');
-
-$anotherCar = PrototypeService::new('\Wheel\Concept\Car\CarProxy');
-//$anotherCar = new \Wheel\Concept\Car\CarProxy();
-
-//$anotherCar->load([
-//    'plate_number' => 'GH-33-17',
-//    'color' => [
-//        'name' => 'Blue',
-//        'code' => 'blue',
-//    ],
-//    'brand' => [
-//        'name' => 'Lada',
-//        'code' => 'lada',
-//    ]
-//]);
-$anotherCar->load($repository->get('car.lada'));
-
-var_dump($anotherCar);
-var_dump($anotherCar->getPlateNumber());
-var_dump($anotherCar->getColor()->getName() . ' (' . $anotherCar->getColor()->getCode() . ')');
-var_dump($anotherCar->getBrand()->getName() . ' (' . $anotherCar->getBrand()->getCode() . ')');
-
-
-
-$t1 = microtime(true);
-$d = round($t1-$t0, 4);
-var_dump($d);
+// TODO 04: Clarify why should we refer to rubric schema with absolute URI (config/schemas/rubricSchema.json#/definitions/rubric) in config/schemas/conceptSchema.json
+// TODO 06: Create commands for generate.php, degenerate.php, repository_test.php(?), publishing.php (needs symfony app)
+// TODO 06: Reorganize directory structure and repos (needs symfony app)
+// TODO 02: Create fixture from concept.json
